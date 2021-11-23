a592ca714176e913329d4be9b9131e1e61e4248a
mrgauz <github@gauz.net>
2021-11-23 16:47:54 +0100

    Sketch matches fragment

---
+++ app/src/main/java/com/example/movieapp/swipe/MatchesFragment.kt
@@ -0,0 +1,30 @@
+package com.example.movieapp.swipe
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.movieapp.R
+import com.example.movieapp.databinding.FragmentMatchesBinding
+import com.example.movieapp.databinding.MatchCardBinding
+
+class MatchesFragment : Fragment() {
+    private lateinit var binding: FragmentMatchesBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        for (i in 0..3) {
+            val matchCardBinding: MatchCardBinding = MatchCardBinding.inflate(layoutInflater)
+            binding.matchesList.addView(matchCardBinding.root)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_matches, container, false)
+    }
+}
