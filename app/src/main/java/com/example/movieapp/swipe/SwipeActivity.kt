f775f8edd61194a8b7512b7e1972753b8889db56
Carlos <ge35wol@mytum.de>
2021-11-23 16:55:48 +0100

    clean up swipe activity

--- app/src/main/java/com/example/movieapp/swipe/SwipeActivity.kt
+++ app/src/main/java/com/example/movieapp/swipe/SwipeActivity.kt
@@ -20,22 +20,5 @@ class SwipeActivity : AppCompatActivity() {
 
         binding = ActivitySwipeBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        setSupportActionBar(binding.toolbar)
-
-        val navController = findNavController(R.id.nav_host_fragment_content_swipe)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
-
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_swipe)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
     }
 }
