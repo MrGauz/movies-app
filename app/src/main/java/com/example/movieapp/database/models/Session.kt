c353d504260c76d5cf52fac766de88fea0af151e
mrgauz <github@gauz.net>
2021-11-24 12:43:30 +0100

    Add missing property

--- app/src/main/java/com/example/movieapp/database/models/Session.kt
+++ app/src/main/java/com/example/movieapp/database/models/Session.kt
@@ -3,6 +3,7 @@ package com.example.movieapp.database.models
 data class Session(
     val id: Int,
     val start_timestamp: Int,
+    var is_active: Boolean,
     var filter: Filter,
     var options: Options,
     var movies: List<List<Movie>>,
