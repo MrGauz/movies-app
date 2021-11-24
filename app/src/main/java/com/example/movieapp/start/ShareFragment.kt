ca09a5efa57c3fc303042fa2b62de66c6a10243d
Carlos <ge35wol@mytum.de>
2021-11-24 10:16:20 +0100

    basic navigation for first fragments done

--- app/src/main/java/com/example/movieapp/start/ShareFragment.kt
+++ app/src/main/java/com/example/movieapp/start/ShareFragment.kt
@@ -5,56 +5,31 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.Navigation
 import com.example.movieapp.R
+import com.example.movieapp.databinding.FragmentDirectorBinding
+import com.example.movieapp.databinding.FragmentShareBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [ShareFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ShareFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
+    private var _binding: FragmentShareBinding? = null
+    private val binding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_share, container, false)
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ShareFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ShareFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        _binding = FragmentShareBinding.inflate(inflater, container, false)
+        val startSwipeButton= binding.startSwiping
+        val shareButton = binding.shareLink
+        startSwipeButton.setOnClickListener{
+            // TODO: 24.11.2021 call the swipe activity with an intent
+            Toast.makeText(this.context, "this function hasnt been implemented yet", Toast.LENGTH_SHORT).show()
+        }
+        shareButton.setOnClickListener{
+            // TODO: 24.11.2021 make intent to share deep link to swipe activity
+            Toast.makeText(this.context, "this function hasnt been implemented yet", Toast.LENGTH_SHORT).show()
+        }
+        val root: View = binding.root
+        return root
     }
 }
--- app/src/main/java/com/example/movieapp/start/StartScreenFragment.kt
+++ app/src/main/java/com/example/movieapp/start/StartScreenFragment.kt
@@ -5,14 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.Navigation
 import androidx.navigation.findNavController
 import com.example.movieapp.R
 import com.example.movieapp.databinding.FragmentStartScreenBinding
 
 class StartScreenFragment : Fragment() {
     private var _binding: FragmentStartScreenBinding? = null
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -20,16 +20,11 @@ class StartScreenFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val view : View = inflater.inflate(R.layout.fragment_start_screen, container, false)
         _binding = FragmentStartScreenBinding.inflate(inflater, container, false)
         val startLobby = binding.startLobby
-        startLobby.setOnClickListener {
-            view.findNavController().navigate(R.id.action_startScreenFragment_to_filterScreenFragment)
-        }
+        startLobby.setOnClickListener (Navigation.createNavigateOnClickListener(R.id.action_startScreenFragment_to_filterScreenFragment))
         val joinLobby = binding.joinLobby
-        joinLobby.setOnClickListener {
-            view.findNavController().navigate(R.id.action_startScreenFragment_to_joinFragment)
-        }
+        joinLobby.setOnClickListener (Navigation.createNavigateOnClickListener(R.id.action_startScreenFragment_to_joinFragment))
         val root: View = binding.root
         return root
     }
