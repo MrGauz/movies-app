1bda88961e24a35f0fdf95768a611d76d65679f2
Carlos <ge35wol@mytum.de>
2021-11-24 10:40:49 +0100

    addded intent to swipe fragment and deleted intent filter from swipe activity in Android Manifest

--- app/src/main/java/com/example/movieapp/start/ShareFragment.kt
+++ app/src/main/java/com/example/movieapp/start/ShareFragment.kt
@@ -1,5 +1,6 @@
 package com.example.movieapp.start
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -10,6 +11,7 @@ import androidx.navigation.Navigation
 import com.example.movieapp.R
 import com.example.movieapp.databinding.FragmentDirectorBinding
 import com.example.movieapp.databinding.FragmentShareBinding
+import com.example.movieapp.swipe.SwipeActivity
 
 class ShareFragment : Fragment() {
     private var _binding: FragmentShareBinding? = null
@@ -23,7 +25,12 @@ class ShareFragment : Fragment() {
         val shareButton = binding.shareLink
         startSwipeButton.setOnClickListener{
             // TODO: 24.11.2021 call the swipe activity with an intent
-            Toast.makeText(this.context, "this function hasnt been implemented yet", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this.context,SwipeActivity::class.java);
+            val userStatus = "admin"
+            val databaseID = "todo" // TODO: 24.11.2021 the database ID has to be passed here somehow
+            intent.putExtra("UserStatus", userStatus)
+            intent.putExtra("DatabaseID", databaseID)
+            startActivity(intent);
         }
         shareButton.setOnClickListener{
             // TODO: 24.11.2021 make intent to share deep link to swipe activity
