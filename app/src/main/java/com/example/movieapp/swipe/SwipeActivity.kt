414f6cce65618ff31f6ea4eef87eada6766c4c0d
Carlos <ge35wol@mytum.de>
2021-11-28 17:09:32 +0100

    fixed error in join fragment sessions can now be created and joined

--- app/src/main/java/com/example/movieapp/swipe/SwipeActivity.kt
+++ app/src/main/java/com/example/movieapp/swipe/SwipeActivity.kt
@@ -8,25 +8,10 @@ import com.example.movieapp.databinding.ActivitySwipeBinding
 
 class SwipeActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySwipeBinding
-    private var isHost: Boolean = false
-    private lateinit var databaseId: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySwipeBinding.inflate(layoutInflater)
-
-        if (intent?.action == "android.intent.action.VIEW") {
-            // User joined via deep link
-            isHost = false
-            databaseId = intent?.data?.getQueryParameter("id").toString()
-            // TODO: move this logic to JoinFragment to check if database ID exists
-        } else {
-            // User started the session or joined from within the app
-            isHost = intent.getBooleanExtra("isHost", false)
-            databaseId = Database.sessionId
-        }
-
-        Toast.makeText(this, "$isHost | $databaseId", Toast.LENGTH_SHORT).show()
 
         setContentView(binding.root)
     }
