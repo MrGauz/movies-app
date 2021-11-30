0634cce6609450e88b8957b7cbd88ff39bb8a553
gauz <github@gauz.net>
2021-11-30 15:57:11 +0100

    Change filter country type

--- app/src/main/java/com/example/movieapp/models/Filter.kt
+++ app/src/main/java/com/example/movieapp/models/Filter.kt
@@ -3,7 +3,7 @@ package com.example.movieapp.models
 data class Filter(
     var genres: MutableList<Long>? = null,
     var releaseYear: ReleaseYearInterval? = null,
-    var country: Country? = null,
+    var country: String? = null,
     var minRating: Double? = null,
     var duration: DurationInterval? = null
 )
