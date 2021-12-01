ff285d943a4184eb9073e8cadf1f44a278b70cd4
gauz <github@gauz.net>
2021-12-01 17:52:58 +0100

    Disable match navigation from details screen

--- app/src/main/java/com/example/movieapp/swipe/DetailsFragment.kt
+++ app/src/main/java/com/example/movieapp/swipe/DetailsFragment.kt
@@ -7,13 +7,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.Navigation
-import androidx.navigation.findNavController
 import com.bumptech.glide.Glide
 import com.example.movieapp.R
 import com.example.movieapp.api.MoviesRepository
-import com.example.movieapp.database.MatchesViewModelFactory
 import com.example.movieapp.models.MovieDetails
 import com.example.movieapp.databinding.FragmentDetailsBinding
 import com.example.movieapp.models.MatchesViewModel
@@ -22,7 +19,6 @@ import com.example.movieapp.models.PosterSize
 class DetailsFragment : Fragment() {
     private var _binding: FragmentDetailsBinding? = null
     private val binding get() = _binding!!
-    private lateinit var matchesViewModel: MatchesViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -34,18 +30,6 @@ class DetailsFragment : Fragment() {
         binding.infoBack.setOnClickListener(
             Navigation.createNavigateOnClickListener(R.id.action_infoFragment_to_swipeFragment)
         )
-
-        // Match matches changes to navigate to matches screen after 3 matches
-        val matchesFactory = MatchesViewModelFactory()
-        matchesViewModel =
-            ViewModelProviders.of(this, matchesFactory).get(MatchesViewModel::class.java)
-        matchesViewModel.getMatches().observe(viewLifecycleOwner, { matches ->
-            // TODO: don't force matches on opening details
-            if (matches.size >= 3) {
-                binding.root.findNavController()
-                    .navigate(R.id.action_infoFragment_to_matchesFragment)
-            }
-        })
 
         // TODO: show "loading details" on the screen,
         //  in case internet connection is slow and details are not loaded instantly
